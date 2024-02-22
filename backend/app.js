24bb921277709a366d1208fa3c61702448cb59b0
dennis iluma <dennis.iluma@sproxil.com>
2024-02-22 22:37:30 +0100

    vue added

--- backend/app.js
+++ backend/app.js
@@ -1,46 +1,45 @@
-const express = require("express")
-const stream = require("stream")
-const multer = require("multer")
-const path = require("path")
-const cors = require("cors")
-const {google} = require("googleapis")
+const stream = require("stream");
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const {google} = require("googleapis");
+const cors = require("cors");
 
-const app = express()
-const upload = multer()
+const app = express();
+const upload = multer();
 
 app.use(express.json())
 app.use(cors())
 
-
 const KEYFILEPATH = path.join(__dirname, "cred.json");
-
-const SCOPE = ["https://www.googleapis.com/auth/drive"]
+const SCOPES = ["https://www.googleapis.com/auth/drive"]
 const auth = new google.auth.GoogleAuth({
     keyFile: KEYFILEPATH,
-    scopes: SCOPE
-})
+    scopes: SCOPES
+});
 
-app.post("/uploadToGoogleDrive", upload.single('image'), async(req, res)=>{
+
+app.post("/uploadToGoogleDrive", upload.single('image'), async (req, res)=>{
     try {
-        const drive = google.drive({version: 'v3', auth:auth})
+        const drive = google.drive({version: "v3", auth:auth})
         const fileMetaData = {
             name: req.file.originalname,
-            parents: ["16_ai2PT7kr9WTSW7IIDVT4utRptfUANs"]
+            parents: ["1I3zBeGBP6FmsetI2DL1l9i_InNNroDQu"]
         }
-        const bufferStream = new stream.PassThrough()
-        bufferStream.end(req.file.buffer)
+
+        const bufferStream = new stream.PassThrough();
+        bufferStream.end(req.file.buffer);
 
         const media = {
             mimeType: req.file.mimetype,
-            body: bufferStream
+            body:bufferStream
         }
         const file = await drive.files.create({
             requestBody: fileMetaData,
             media
-        })
+        });
 
-
-       // Generate the publicly accessible URL
+               // Generate the publicly accessible URL
        const imageUrl1 = `https://drive.google.com/uc?export=view&id=${file.data.id}`; //use as user content for public use
        const imageUrl2 = `https://drive.google.com/file/d/${file.data.id}` // view as google drive image
        console.log("URL 1: ", imageUrl1)
@@ -49,10 +48,10 @@ app.post("/uploadToGoogleDrive", upload.single('image'), async(req, res)=>{
 
     } catch (error) {
         console.log("ERROR UPLOADING IS: ", error.message);
-       res.status(500).json({ status: 'error', statusCode: 500, message: error.message || 'Failed to save image to Drive' });
-
+        res.status(500).json({ status: 'error', statusCode: 500, message: error.message || 'Failed to save image to Drive' });
+ 
     }
-})
+} )
 
 
 
